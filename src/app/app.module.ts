e95ae94dcdf5f196694cdcae896d8d406e361225
Daniel <danielkolotashvilio@gmail.com>
2024-12-22 22:43:13 +0400

    little changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from '@app/app.component';
 import { provideHttpClient } from '@angular/common/http';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavBarComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideHttpClient()],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -3,10 +3,8 @@ import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  standalone: true,
-  imports: [RouterModule],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
+  styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent {
 
